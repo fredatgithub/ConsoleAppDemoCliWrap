aaf56dd409463f1e6bdda091b85b8b0f85f2cc3f
fred <domaine75@gmail.com>
2023-02-18 12:43:59 +0100

    Ajoutez des fichiers projet.

---
+++ ConsoleAppDemoCliWrap/Program.cs
@@ -0,0 +1,29 @@
+﻿using CliWrap;
+using CliWrap.Buffered;
+using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(6));
+
+// example 1
+//await Cli.Wrap("notepad")
+//  //.WithArguments("toto.txt")
+//  .WithArguments(arguments => arguments
+//    .Add("toto.txt"))
+//  .WithStandardOutputPipe(PipeTarget.ToDelegate(Console.WriteLine))
+//  .WithStandardOutputPipe(PipeTarget.ToFile("log.log"))
+//  //.ExecuteAsync();
+//  .ExecuteBufferedAsync();
+
+// example 2
+try
+{
+  Cli.Wrap("ping")
+   .WithArguments("-t google.com")
+   .WithStandardOutputPipe(PipeTarget.ToDelegate(Console.WriteLine))
+   .ExecuteAsync(cts.Token);
+}
+catch (OperationCanceledException)
+{
+  Console.WriteLine("cancelled");
+}
+
+Console.WriteLine("press any key to exit:");
+Console.ReadKey();
---
+++ ConsoleAppDemoCliWrap472/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using CliWrap;
+
+namespace ConsoleAppDemoCliWrap472
+{
+  internal class Program
+  {
+    static  void Main(string[] args)
+    {
+      try
+      {
+        var cts = new CancellationTokenSource(TimeSpan.FromSeconds(6));
+        Cli.Wrap("ping")
+         .WithArguments("-t google.com")
+         .WithStandardOutputPipe(PipeTarget.ToDelegate(Console.WriteLine))
+         .ExecuteAsync(cts.Token);
+      }
+      catch (OperationCanceledException)
+      {
+        Console.WriteLine("cancelled");
+      }
+
+      Console.WriteLine("Press any key to exit:");
+      Console.ReadKey();
+    }
+  }
+}
